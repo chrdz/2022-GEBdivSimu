6d2e2414e794dc126081885ea928f1f57b4b1af6
chrdz <rdz.charlotte@gmail.com>
2022-02-09 08:29:25 +0100

    to save last image of video created by movie centerline

---
+++ save_images.m
@@ -0,0 +1,4 @@
+
+fig = movieCenterline(p2, R2, 1:1000, viewCent, locLegend, titleCenterline, t);
+title('Random title', 'Color','none')
+exportgraphics(fig,'flying_spaghetti.pdf','ContentType','vector')
